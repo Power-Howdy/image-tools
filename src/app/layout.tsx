1336b2c633eb9c27f2a644f75bc90f61dbed7c1e
Mad Devil Coder <giftedclan0305@gmail.com>
2026-03-22 10:42:42 +0000

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className="antialiased font-sans">
         {children}
         <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+        <Analytics />
       </body>
     </html>
   );
